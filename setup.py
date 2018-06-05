7cb6786853500261b632c822e6dee042b18ae228
Billy Olsen <billy.olsen@gmail.com>
2018-06-05 14:20:28 -0700

    Change setup.py to load deps from requirements.txt
    It is possible for the requirements.txt and the setup.py to differ
    and not be updated. This duplication should be removed before it
    eventually causes problems.
    
    Signed-off-by: Billy Olsen <billy.olsen@gmail.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 from setuptools import setup, find_packages
 
 
-dependencies = ["lxml", ]
+def get_dependencies():
+    """Reads the dependencies from the requirements file."""
+    with open('requirements.txt', 'r') as d:
+        dependencies = d.read()
+
+    return dependencies
+
+
+def get_version():
+    """Returns the version as a relative commit to a tag and version"""
+    pass
+
 
 setup(
     name="cmadison",
@@ -14,7 +24,7 @@ setup(
     author_email="billy.olsen@gmail.com",
     description=("A wrapper for rmadison including basic support for "
                  "Ubuntu Cloud Archive"),
-    install_requires=dependencies,
+    install_requires=get_dependencies(),
     packages=find_packages(),
     url='http://github.com/wolsen/cmadison',
     entry_points={
@@ -23,7 +33,7 @@ setup(
             ]
         },
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
     ]
